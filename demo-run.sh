d1d9fc4bbcdf9c0206380a9609c1e66ae27dedcc
Oleksandr Kharchenko <okharch@gmail.com>
2023-04-19 17:12:30 +0300

    Additions and improvements to the script that runs and manages the demonstration of the service.

--- demo-run.sh
+++ demo-run.sh
@@ -1,5 +1,10 @@
+# make sure to remove existing service to run the latest
 docker service rm my-service
+# create secret in a case if it does not exists
 printf "This is a secret" | docker secret create MySecret -
+# build docker image my-service
 docker build -t my-service .
+# creates service my-service from image my-service and runs it
 docker service  create --name my-service --secret MySecret my-service
+# output logs of the service
 docker service logs -f my-service
